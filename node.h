f37037b9a395ca55f0cf240dc74925d9f1c7b27e
Wade Liu <weichih654@gmail.com>
2016-05-26 20:11:10 +0800

    Add function declare in header

--- node.h
+++ node.h
@@ -18,5 +18,7 @@ typedef struct node
 } NODE;
 
 NODE* create_node (char* name);
+void append_child (NODE* parent, NODE* child);
 void dump_node (NODE* node);
+void get_full_path (NODE* leaf_node, char* fullpath, int path_len);
 #endif //NODE_HEADER
